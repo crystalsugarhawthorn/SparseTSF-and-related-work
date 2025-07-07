0ef1cc9a658d5b788b0d132fdb6667d94b573029
高景珩 <153498512+crystalsugarhawthorn@users.noreply.github.com>
2025-07-07 17:06:01 +0800

    Delete Mindspore改进 directory

--- Mindspore改进/SparseTSF_mindspore_filemood/data_factory/data_Standardization.py
+++
@@ -1,6 +0,0 @@
-from data_factory import data_loader
-from sklearn.preprocessing import StandardScaler
-
-# 标准化 data
-scaler = StandardScaler()
-data = scaler.fit_transform(data_loader.raw_data)  # 标准化后的数据
